b8a2bcf158419627a64e1818554996ed870198bc
viphavethcm <loivang.nhatduy@gmail.com>
2023-02-11 01:09:18 +0700

    init project

---
+++ inv-account-svc/src/test/java/com/stock/microservices/invaccountsvc/InvAccountSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.microservices.invaccountsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvAccountSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
