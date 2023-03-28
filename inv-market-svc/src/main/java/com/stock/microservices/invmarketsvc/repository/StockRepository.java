502d12b29dd4ae3ebd9df2b3e7f7b49d9c4e1ad1
viphavethcm <loivang.nhatduy@gmail.com>
2023-03-28 12:20:14 +0700

    - add r2dbc library

--- inv-market-svc/src/main/java/com/stock/microservices/invmarketsvc/repository/StockRepository.java
+++ inv-market-svc/src/main/java/com/stock/microservices/invmarketsvc/repository/StockRepository.java
@@ -2,8 +2,9 @@ package com.stock.microservices.invmarketsvc.repository;
 
 import com.stock.microservices.invmarketsvc.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, String> {
+public interface StockRepository extends ReactiveCrudRepository<Stock, String> {
 }
